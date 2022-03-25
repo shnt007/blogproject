067a18def2c7f4aca7dff84845aeed76ca348c1d
shnt007 <sushantd34@gmail.com>
2022-03-25 09:21:28 +0545

    User Update Created and Implemented

--- myproject/users/models.py
+++ myproject/users/models.py
@@ -1,14 +1,18 @@
+from email.policy import default
 from django.db import models
-
+from django.contrib.auth.models import User
+#from PIL import Image
 # Create your models here.
 
 
-'''class User(models.Model):
-    name = models.CharField(max_length=50)
-    contact = models.CharField(max_length=20)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-    class Meta:
-        db_class = 'users'
-'''
+    def __str__(self):
+        return f'{self.user.username}Profile'
+
+    '''def save(self):
+        super().save()
+
+        img=Image.open(self.image.path)'''
